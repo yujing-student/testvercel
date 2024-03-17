74b64f83f28101cccc6eccbcf366d536432909cf
yujing <medischdossier03@gmail.com>
2024-03-17 14:21:06 +0100

    test

--- api/index.js
+++ api/index.js
@@ -4,12 +4,10 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../views'))
-// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
 	res.render('index', {
-		text: 'Vercel met EJS en Node.test :)',
+		text: 'Vercel met EJS en Node. For free :)',
 	})
 })
 
