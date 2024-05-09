aaa1bff6f50babfbace5b0315436539c2e1bd59c
yujing <medischdossier03@gmail.com>
2024-05-09 17:37:24 +0200

    aanpassing index

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '../views'))
+app.set('views', path.join(__dirname, './views'))
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}));//deze regel code gebruiken vanwege middelware zodat de data leesbaar gemaakt word
